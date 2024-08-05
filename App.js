30602a06a441ba084248a1f522e21cc152a9ff7b
Neal Langhorne <126914856+neallang@users.noreply.github.com>
2024-08-05 16:27:54 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react"
-import Sidebar from "./components/Sidebar"
-import Editor from "./components/Editor"
+import Sidebar from "./Sidebar"
+import Editor from "./Editor"
 import { data } from "./data"
 import Split from "react-split"
 import {nanoid} from "nanoid"
